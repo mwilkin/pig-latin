43c991d84eb43ae2d85d8db1172e8cd9df433966
Carl Egbert and Matt Wilkin <matt@>
2016-06-08 15:07:45 -0700

    add functionality for translating multiple words at once.

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,6 @@ var vowels = "aeiou";
 var translateWord = function(string) {
 
   var isVowel = function(char) {
-
     for(var index = 0; index < vowels.length; index++) {
       if (char === vowels[index]) return true;
     }
@@ -36,12 +35,27 @@ var translateWord = function(string) {
 
 }
 
+var translatePhrase = function(phrase) {
+  var wordArray = phrase.split(/\W+/);
+  var translatedArray = wordArray.map(function(word){
+    return translateWord(word);
+  });
+  var resultString = "";
+  translatedArray.forEach(function(word){
+    resultString+=word;
+    resultString+=" ";
+  });
+  var firstLetter = resultString.charAt(0).toUpperCase();
+  resultString = firstLetter + resultString.slice(1,(resultString.length-1))+".";
+  return resultString;
+}
+
 
 // Front-end code
 $(document).ready(function(){
   $("form").submit(function(event){
     event.preventDefault();
     var textInput = $("#textInput").val();
-    $("#results").append("<p>"+translateWord(textInput)+"</p>");
+    $("#results").append("<p>"+translatePhrase(textInput)+"</p>");
   });
 });
