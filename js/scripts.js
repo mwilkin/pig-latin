c7a6989dbcfd3e2b7c3635959c1eb68131779e6c
Carl Egbert and Matt Wilkin <matt@>
2016-06-08 14:48:08 -0700

    working on making sentence translations work

--- js/scripts.js
+++ js/scripts.js
@@ -3,36 +3,56 @@ var vowels = "aeiou";
 
 var translateWord = function(string) {
 
-  var isVowel = function(char) {
-
-    for(var index = 0; index < vowels.length; index++) {
-      if (char === vowels[index]) return true;
-    }
-    return false;
-  }
-
-  var isQU = function() {
-    if (string[0] === "q" && string[1] === "u") return true;
-    else return false;
-  }
-
-  var moveChar = function() {
-    if (!isVowel(string[0])) {
-      if (isQU()) {
-        string+=string.slice(0,2);
-        string = string.slice(2);
-      } else {
-        string += string[0];
-        string = string.slice(1);
-      }
-      vowels+="y";
-      moveChar();
-    }
-  }
-
   string = string.toLowerCase();
-  moveChar();
+  moveChar(string);
   return string + "ay";
+
+
+
+}
+var isQU = function(string) {
+  if (string[0] === "q" && string[1] === "u") return true;
+  else return false;
+}
+
+var isVowel = function(char) {
+  for(var index = 0; index < vowels.length; index++) {
+    if (char === vowels[index]) return true;
+  }
+  return false;
+}
+
+var moveChar = function(string) {
+  if (!isVowel(string[0])) {
+    if (isQU(string)) {
+      string+=string.slice(0,2);
+      string = string.slice(2);
+    } else {
+      string += string[0];
+      string = string.slice(1);
+    }
+    vowels+="y";
+    moveChar(string);
+  }
+}
+
+var checkIfWord = function(word) {
+  var firstLetter = word[0].toLowerCase;
+  if ((firstLetter => 'a') && (firstLetter < 'z')) {
+    return true;
+  } else return false;
+}
+
+var translatePhrase = function(phraseInput) {
+  var arr = phraseInput.replace(/[^\w\s]|_/g, function ($1) { return ' ' + $1 + ' ';}).replace(/[ ]+/g, ' ').split(' ');
+  var translatedArray = "";
+  console.log(arr);
+
+  arr.forEach(function(word) {
+    if (checkIfWord(word)) {
+      translatedArray += translateWord(word);
+    }
+  });
 
 }
 
@@ -42,6 +62,6 @@ $(document).ready(function(){
   $("form").submit(function(event){
     event.preventDefault();
     var textInput = $("#textInput").val();
-    $("#results").append("<p>"+translateWord(textInput)+"</p>");
+    $("#results").append("<p>"+translatePhrase(textInput)+"</p>");
   });
 });
